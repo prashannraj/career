061e036196a35b187c49500fdd2313135d7659f2
Prashann Raj Karn <prashannraj@hotmail.com>
2023-11-21 08:53:35 +0545

    home page created

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,20 +2,12 @@
 import Image from 'next/image'
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
+import {  message } from 'antd';
 import * as Yup from 'yup';
-
-const SignupSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
-  password: Yup.string().required('Required')
-});
 
 
 const NavBar = () =>{
-  const NavItem = [<a href="">Home</a>,<a href="">Vacancies</a>,<a href="">Gallery</a>,<a href="">Contact</a>]
+  const NavItem = [<a href="/">Home</a>,<a href="/register">Register</a>,<a href="/login">Login</a>,<a href="">Contact</a>]
   return(
    <div className='navbar'>
     {NavItem.map((item, id)=>{
@@ -30,6 +22,7 @@ const NavBar = () =>{
 }
 
 export default function Home() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -65,42 +58,9 @@ export default function Home() {
         />
       </div>
       <div className='registration'>
-        <h1>Signup First</h1>
+        <h1>This is Home Page</h1>
     <br></br>
-    <Formik
-      initialValues={{
-        name: '',
-        email: '',
-        password:''
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={values => {
-        // same shape as initial values
-        console.log(values);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form >
-         
-         <Field name="name" type="name" placeholder="Enter your Name" />
-          {errors.name && touched.name ? <div>{errors.name}</div> : null}
-          <br />
-          <br />
-          <Field name="email" type="email" placeholder="Enter your email" />
-          {errors.email && touched.email ? <div>{errors.email}</div> : null}
-          <br />
-          <br />
-          <Field name="password" type="password" placeholder="Enter your password"/>
-          {errors.password && touched.password? <div>{errors.password}</div> : null}
-          <br />
-          <br />
-          <button type="submit" className='button'>Submit</button>
-          <br />
-          if you don"t have account
-          {/* <br /><Link href="./register">go to register</Link> */}
-        </Form>
-      )}
-    </Formik>
+    
   </div>
        
      
